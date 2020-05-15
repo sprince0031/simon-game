4d0a9779785381ba1c6888e54d6114fa22b43157
sprince0031 <siddharthprince31@gmail.com>
2020-05-15 13:25:09 +0530

    Added a couple of cheats 🔥

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ let userPattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
 let level = 0;
 let gameStart = true;
+let cheatString = "";
+let showLogsCheat = false;
+let godModeCheat = false;
+let speed = 1000;
+// let clearCheatOnInterval = setInterval(function() {
+//     cheatString = "";
+// }, 5000);
+
+$(document).ready(function() {
+    $(".cheat-activated").hide();
+});
 
 $(".btn").on("click", function () {
     let userChoice = $(this).attr("id");
@@ -12,19 +23,53 @@ $(".btn").on("click", function () {
         $("#"+userChoice).removeClass("pressed");
     }, 100);
     userPattern.push(userChoice);
-    // console.log("user: " + userPattern);
+    // if (showLogsCheat) {
+    //     console.log("User entered sequence: \n" + userPattern);
+    // }
 
     checkAnswer(userPattern.length - 1);
     
 });
 
 $(document).on("keydown", function(event) {
+
+    if (event.key.length === 1 && event.key.match(/[a-z]/i)) {
+        cheatString += event.key;
+        if (cheatString.length === 1) {
+            setTimeout(function() {
+                cheatString = "";
+            }, 5000);
+        } else if (cheatString.length > 8) {
+            cheatString = "";
+        }
+        
+        if (cheatString === "showlogs") {
+            showLogsCheat = cheatToggler(showLogsCheat);
+            if (showLogsCheat) {
+                console.log("Sequence to complete: \n" + generatedPattern);
+            }
+        }
+
+        if (cheatString === "godmode") {
+            godModeCheat = cheatToggler(godModeCheat);
+            godMode(godModeCheat);
+        }
+    }
+
     if (gameStart && event.key === "Enter") {
         genSequence();
         gameStart = false;
-        $("#level-title").text("Level " + level);
+        if (!godModeCheat) {
+            $("#level-title").text("Level " + level);
+        } else {
+            $("#level-title").html("Level &infin;");
+        }
+
     }
+
 });
+
+
 
 function chooseSoundEffect(soundFile) {
     new Audio("./sounds/" + soundFile + ".mp3").play();
@@ -35,24 +80,31 @@ function genSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     let colourChoice = buttonColours[randomNumber];
     generatedPattern.push(colourChoice);
-    // console.log("generated: " + generatedPattern);
+
+    if (showLogsCheat) {
+        console.log("Sequence to complete: \n" + generatedPattern);
+    }
+    
     $("#"+colourChoice).fadeOut(100).fadeIn(100);
     chooseSoundEffect(colourChoice);
 
-    level++;
-    $("#level-title").text("Level " + level);
+    if (!godModeCheat) {
+        level++;
+        $("#level-title").text("Level " + level);
+    }
+
 
 }
 
 function checkAnswer(index) {
 
     if (generatedPattern[index] === userPattern[index]) {
-        console.log("correct answer!")
+        console.log("Correct answer!")
 
         if (index === generatedPattern.length - 1) {
             setTimeout(function() {
                 genSequence();
-            }, 1000);
+            }, speed);
         }
 
     } else {
@@ -63,10 +115,77 @@ function checkAnswer(index) {
         }, 100);
         chooseSoundEffect("wrong");
         $("#level-title").text("Game Over, Press Enter to Restart");
-        gameStart = true;
-        generatedPattern = [];
-        userPattern = [];
-        level = 0;
+        resetGame();
     }
 
+}
+
+function resetGame() {
+    gameStart = true;
+    generatedPattern = [];
+    userPattern = [];
+    level = 0;
+}
+
+/*
+*   Cheat codes because they are dank af... 8|
+*/
+
+function cheatToggler(cheatMode) {
+    let returnMode;
+    if (cheatMode) {
+        console.log("Cheat Deactivated!");
+        $(".cheat-activated").text("Cheat deactivated");
+        
+        returnMode = false;
+    } else {
+        console.log("Cheat Activated!");
+        $(".cheat-activated").text("Cheat activated");
+        
+        returnMode = true;
+    }
+    chooseSoundEffect("cheat");
+    $(".cheat-activated").fadeIn(500);
+    setTimeout(function() {
+        $(".cheat-activated").fadeOut(500);
+    }, 2000);
+    cheatString = "";
+
+    return returnMode;
+
+}
+
+let godModeBgm = new Audio("./sounds/god-mode-san-bgm.mp3");
+function godMode(godModeCheat) {
+    
+    if (godModeCheat) {
+        $("#level-title").html("Level &infin;").css("color", "#E25822").animate({
+            fontSize: "+=150px",
+        }, 1000);
+
+        $(".btn").addClass("god-mode");
+        $("body").addClass("body-god-mode");
+        speed = 200;
+        $("#favicon").attr("href", "./images/thug-life.jpg");
+        $("title").text("🔥🔥🔥 GOD MODE 🔥🔥🔥")
+        godModeBgm.play();
+    } else {
+        let titleText;
+        if (level === 0) {
+            titleText = "Press Enter to Start";
+        } else {
+            titleText = "Level " + level;
+        }
+        $("#level-title").text(titleText).css("color", "#FEF2BF").animate({
+            fontSize: "-=150px",
+        }, 1000);
+        gameStart = true;
+        $(".btn").removeClass("god-mode");
+        $("body").removeClass("body-god-mode");
+        speed = 1000;
+        $("#favicon").attr("href", "./images/simon-favicon.png");
+        $("title").text("Simon Game | By sprince0031")
+        godModeBgm.pause();
+    }
+    resetGame();
 }
